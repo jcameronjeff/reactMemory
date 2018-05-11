c57e0bfd757bbcdce0dfa7707e430cfeab429011
Jeff Cameron <jcameronjeff@gmail.com>
2018-05-11 12:36:28 -0400

    final

--- client/src/components/SearchResultContainer.js
+++ client/src/components/SearchResultContainer.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import SearchResults from "./SearchResults";
 import API from "../utils/API";
 import shuffleArr from "../utils/shuffleArr"
- 
-  
+
 class SearchResultContainer extends Component {
   state = {
     score: 0,
     picked: [],
-    trucks: []
+    trucks: [],
+    status: "Don't Click it Twice!"
   };
 
   // When this component mounts, search the Giphy API for pictures of kittens
@@ -17,32 +17,53 @@ class SearchResultContainer extends Component {
   }
 
   searchPixabay = query => {
-    API.search(query)
-      .then(res => {this.setState({ trucks: res.data.hits }); console.log(this.state.trucks);})
+    API
+      .search(query)
+      .then(res => {
+        this.setState({trucks: res.data.hits});
+        console.log(this.state.trucks);
+      })
       .catch(err => console.log(err));
   };
 
-
   handleClickPic = event => {
-    console.log(this);
+    let pic = event.target.alt;
+    console.log(pic)
     this.handleShufflePic();
+    this.handleScore(pic);
   }
-  
+
+  handleScore = event => {
+
+    if (this.state.picked.includes(event)) {
+      this.setState({status: "You Loose!"})
+    } else {
+      this.setState({
+        picked: this
+          .state
+          .picked
+          .concat(event)
+      })
+
+      this.setState({score: this.state.picked.length})
+    }
+  }
 
   handleShufflePic = () => {
     let trucksArr = this.state.trucks;
     let shuffledArr = shuffleArr(trucksArr);
-    this.setState({ trucks: shuffledArr });
+    this.setState({trucks: shuffledArr});
   }
 
-  render(){
-      return(
-        <div>
-        <SearchResults trucks={this.state.trucks} handleClickPic={this.handleClickPic} />
-        </div>
-         )
+  render() {
+    return (
+      <div>
+        <h1>Score: {this.state.score}</h1>
+        <h2>{this.state.status}</h2>
+        <SearchResults trucks={this.state.trucks} handleClickPic={this.handleClickPic}/>
+      </div>
+    )
   }
 }
-
 
 export default SearchResultContainer;
--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { GridList, GridTile } from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList';
 import PropTypes from 'prop-types';
-
 
 const SearchResults = props => (
     <GridList cols={3}>
-  
-    {props.trucks.map(truck => (
-        <GridTile key={truck.id}>
-        <img alt={truck.id} onClick={(event)=>{props.handleClickPic();}} src={truck.largeImageURL} height='300px'/>
-        </GridTile>
-    ))}
-    
+
+        {props
+            .trucks
+            .map(truck => (
+                <GridTile key={truck.id}>
+                    <img
+                        alt={truck.id}
+                        onClick={props.handleClickPic}
+                        src={truck.largeImageURL}
+                        height='300px'/>
+                </GridTile>
+            ))}
+
     </GridList>
 );
 
-SearchResults.propTypes ={
+SearchResults.propTypes = {
     trucks: PropTypes.array.isRequired
 }
 export default SearchResults;
